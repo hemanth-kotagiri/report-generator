d8ebd592fa03efa04f8ad689606085280f20ba0e
Hemanth <hemanth.kotagiri43@gmail.com>
2020-09-09 15:15:53 +0530

    Add Update Date Button - Fully Functioning

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import os
+from datetime import date
 from kivy.app import App
 from kivy.uix.button import Button
+from openpyxl import load_workbook
 
 
-class TestApp(App):
-    "Test Application"
+# Loading the workbook from the current directory
+wb = load_workbook(os.path.join(os.getcwd(), "4-08-2020 (2).xlsx"))
+
+#TODO: Check if the date is updated already
+
+class FirstApp(App):
+
+    def update_date(self, obj):
+        print("Updating...")
+
+        sheet1 = wb["Circle 6"] # hardcoding the sheetname for testing purposes
+        today = date.today().strftime("%d-%m-%Y")
+        sheet1["K2"].value = today 
+        wb.save("4-08-2020 (2).xlsx")
+        print("DATE UPDATED")
+
 
     def build(self):
-        b1 = Button(text = "Test Button")
-        return b1
+        b = Button(text = "Update Date", on_press = self.update_date)
+        return b
 
 
-TestApp().run()
+FirstApp().run()
