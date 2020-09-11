415016ff9c4e455c4c35eff6ec4bcb0439a225eb
Hemanth <hemanth.kotagiri43@gmail.com>
2020-09-11 14:06:44 +0530

    Complete TODO, update 'update_date' function to avoid redundantly updating date in new API

--- api.py
+++ api.py
@@ -12,18 +12,24 @@ class EditAPI:
         self.workbook_name = workbook_name
         self.workbook = load_workbook(workbook_name)
         self.sheet = self.workbook[sheet_name]
+        self.date_updated = False
         logging.info("Workbook loaded successfully")
 
 
     def update_date(self, instance):
         """ Updates the date in a given sheet of the workbook """
-
+        if self.date_updated:
+            logging.info("DATE ALREADY UPDATED")
+            self.workbook.save(self.workbook_name)
+            return
+        
         today = date.today().strftime("%d-%m-%Y")
         update_date = "Date: " + str(today)
         logging.info("Updating Date: {}".format(update_date))
         self.sheet["K2"].value = update_date
+
+        self.date_updated = True
         logging.info("DATE UPDATED SUCCESSFULLY")
-        print("Updated")
         self.workbook.save(self.workbook_name)
 
 
