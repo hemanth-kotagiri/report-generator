f6409cb429e237898537f7e4ba2b0faa6d017ef3
Hemanth <hemanth.kotagiri43@gmail.com>
2020-09-11 14:33:37 +0530

    Complete TODO, add Opening Screen: SelectCircles in API

--- api.py
+++ api.py
@@ -2,13 +2,31 @@ import logging
 from datetime import time
 from datetime import date
 from openpyxl import load_workbook
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
-#TODO: Add updated state in update_data method
+
+class SelectCircles(GridLayout):
+    def __init__(self, **kwargs):
+        """ Opening Page of the App """
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(Label(text = "Click on any Circle to edit"))
+        self.add_widget(Button(text = "Circle 6"))
+        self.add_widget(Button(text = "Circle 7"))
+        self.add_widget(Button(text = "Circle 8"))
+        self.add_widget(Button(text = "Circle 9"))
+        self.add_widget(Button(text = "Circle 10"))
+        self.add_widget(Button(text = "Circle 11"))
+
 
 class EditAPI:
     """ This contains all the functions to edit a cell in a sheet """
 
     def __init__(self, workbook_name, sheet_name):
+        """ workbook_name = name of the workbook, sheet_name = name of the sheet to edit """
+
         self.workbook_name = workbook_name
         self.workbook = load_workbook(workbook_name)
         self.sheet = self.workbook[sheet_name]
@@ -18,6 +36,7 @@ class EditAPI:
 
     def update_date(self, instance):
         """ Updates the date in a given sheet of the workbook """
+
         if self.date_updated:
             logging.info("DATE ALREADY UPDATED")
             self.workbook.save(self.workbook_name)
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import logging
 import os
 from datetime import date
-from kivy.app import App
-from kivy.uix.button import Button
 from openpyxl import load_workbook
 from api import EditAPI
+from api import SelectCircles
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
 
 
 # Loading the workbook from the current directory
 workbook = load_workbook(os.path.join(os.getcwd(), "4-08-2020 (2).xlsx"))
 
-class FirstApp(App):
+class ReportGenerator(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.sheet6_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 6")
@@ -23,9 +25,9 @@ class FirstApp(App):
     def build(self):
 
         # Updating date for sheet 6
-        b = Button(text = "Update Date")
-        b.bind(on_press = self.sheet6_editor.update_date)
-        return b
+        # b = Button(text = "Update Date")
+        # b.bind(on_press = self.sheet6_editor.update_date)
+        return SelectCircles()
 
 
-FirstApp().run()
+ReportGenerator().run()
