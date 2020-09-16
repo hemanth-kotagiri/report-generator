1abf8983e09a5958c0507ac98c40e1d1ab6f50dd
Hemanth <hemanth.kotagiri43@gmail.com>
2020-09-17 02:11:39 +0530

    Convert entire codebase to kv

--- api.py
+++ api.py
@@ -1,235 +1,98 @@
 import logging
 import os
-from datetime import time
-from datetime import date
+from datetime import date, time
 from openpyxl import load_workbook
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.lang import Builder
+ 
+from kivy.uix.recycleview import RecycleView
 from kivy.properties import ObjectProperty
+from kivy.lang import Builder
 from kivy.app import App
-
-from android.storage import primary_external_storage_path                      
+ 
+from android.storage import primary_external_storage_path
 primary_ext_storage = primary_external_storage_path()
-from android.permissions import request_permissions, Permission                
+from android.permissions import request_permissions, Permission
 request_permissions([Permission.WRITE_EXTERNAL_STORAGE])
-
-
-class InfoLabel(Label):
-    pass
-
-class AreaInput(TextInput):
-    pass
-
-
-class IndividualEdit(GridLayout):
+ 
+ 
+class IndividualEdit(RecycleView):
     """ The next page to edit individual sheets """
     sm = ObjectProperty(None)
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 1
-        self.padding =20
-        self.spacing = 50
-        self.bind(minimum_height=self.setter('height'))
-
-        # Initializing all the sheets
-
-        self.sheet6_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 6")
-        self.sheet7_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 7")
-        self.sheet8_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 8")
-        self.sheet9_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 9")
-        self.sheet10_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 10")
-        self.sheet11_editor = EditAPI("4-08-2020 (2).xlsx", "Circle 11")
-
-        self.sheet_editors = {
-            "Circle 6": self.sheet6_editor,
-            "Circle 7": self.sheet7_editor,
-            "Circle 8": self.sheet8_editor,
-            "Circle 9": self.sheet9_editor,
-            "Circle 10": self.sheet10_editor,
-            "Circle 11": self.sheet11_editor
-        }
-
-
-    def update_date(self):
-        self.sheet_editors[self.sm.circle].update_date()
-
-    def go_back(self):
-        self.sm.current = "Main Page"
-
-    def on_pre_enter(self, *args):
-        self.clear_widgets()
-
-        self.vmf_name = []
-        self.pfm_1_km = []
-        self.pfm_2_km = []
-        self.vmf_km = []
-        self.pfm_1_area = []
-        self.pfm_2_area = []
-        self.vmf_area = []
-        self.my_rows = []
-
+ 
+    def init_data(self, *args):
+        app = App.get_running_app()
+        self.data = []
         for i in range(5, 12):
-            # PFM-1 Input fields
-            try:
-                if len(self.sheet_editors[self.sm.circle].sheet["B" + str(i)].value.split("-")) == 1: continue
+            if app.sheet_editors[app.circle].sheet[f"B{i}"].value.title() != "Total":
+                self.data.append(
+                    {"l1":app.sheet_editors[app.circle].sheet[f"B{i}"].value or "",
+                      "l2":app.sheet_editors[app.circle].sheet[f"C{i}"].value or "",
+                      "l3":app.sheet_editors[app.circle].sheet[f"F{i}"].value or "",
+                      "km_input1": "KM",
+                      "area_input1": "Areas Covered",
+                      "km_input2": "KM",
+                      "area_input2": "Areas Covered",
+                      "vmf_input": "Name",
+                      "vmf_km_input": "KM",
+                      "vmf_area_input": "Areas Covered",
+                      "index": i-5}
+                )
+            else:
+                break
 
-                if self.sheet_editors[self.sm.circle].sheet["B" + str(i)].value.split("-")[1].isnumeric() or self.sheet_editors[self.sm.circle].sheet["B" + str(i)].value == "Complaint Machine":
-                    row = BoxLayout(orientation="vertical", size_hint_y=None, height=350)
-                    pfm_1_layout = BoxLayout(size_hint_y=1)
-                    pfm_2_layout = BoxLayout(size_hint_y=1)
-                    vmf_layout = BoxLayout(size_hint_y=1)
-
-                    divison_label = Label(text=self.sheet_editors[self.sm.circle].sheet["B" + str(i)].value or "", color=(0,0,0,1))
-                    self.add_widget(divison_label)
-
-                    pfm1_info_label = InfoLabel(text="PFM-1")
-                    pfm_1_layout.add_widget(pfm1_info_label)
-
-                    pfm2_info_label = InfoLabel(text="PFM-2")
-                    pfm_2_layout.add_widget(pfm2_info_label)
-
-                    vmf_info_label = InfoLabel(text="VMF   ")
-                    vmf_layout.add_widget(vmf_info_label)
-
-                    name_mobile_label = Label(text=self.sheet_editors[self.sm.circle].sheet["C" + str(i)].value or "")
-                    pfm_1_layout.add_widget(name_mobile_label)
-
-                    name_mobile_label_2 = Label(text=self.sheet_editors[self.sm.circle].sheet["F" + str(i)].value or "")
-                    pfm_2_layout.add_widget(name_mobile_label_2)
-
-                    vmf_name_in = AreaInput(text="Name", width = 5)
-                    self.vmf_name.append(vmf_name_in)
-                    vmf_layout.add_widget(vmf_name_in)
-
-                    km_input = AreaInput(text="KM")
-                    self.pfm_1_km.append(km_input)
-                    pfm_1_layout.add_widget(km_input)
-
-                    km_input_2 = AreaInput(text="KM")
-                    self.pfm_2_km.append(km_input_2)
-                    pfm_2_layout.add_widget(km_input_2)
-
-                    vmf_km_in = AreaInput(text="KM")
-                    self.vmf_km.append(vmf_km_in)
-                    vmf_layout.add_widget(vmf_km_in)
-
-                    area_input = AreaInput(text="Areas Covered")
-                    self.pfm_1_area.append(area_input)
-                    pfm_1_layout.add_widget(area_input)
-
-                    area_input_2 = AreaInput(text="Areas Covered")
-                    self.pfm_2_area.append(area_input_2)
-                    pfm_2_layout.add_widget(area_input_2)
-
-                    vmf_area = AreaInput(text="Area")
-                    self.vmf_area.append(vmf_area)
-                    vmf_layout.add_widget(vmf_area)
-
-                    row.add_widget(pfm_1_layout)
-                    row.add_widget(pfm_2_layout)
-                    row.add_widget(vmf_layout)
-                    self.add_widget(row)
-                    self.my_rows.append([pfm_1_layout, pfm_2_layout, vmf_layout])
-                else:
-                    continue
-            except Exception as e:
-                logging.info(e)
-
-    def on_parent(self, *args):
-        self.parent.bind(on_pre_enter=self.on_pre_enter)
-
+ 
     def update_all(self):
-        # Updating the KM values for PFM
-        for area_list, rowname in [(self.pfm_1_km, "D"),(self.pfm_2_km, "G")]:
-            for i, area in enumerate(area_list):
-                self.sheet_editors[self.sm.circle].sheet[f"{rowname}{i+5}"].value = self.check_string_km(area.text)
-
-        # Updating the Areas covered for PFMs and VMFs
-        for area_list, rowname in [(self.pfm_1_area, "E"), (self.pfm_2_area, "H"), (self.vmf_km, "J"), (self.vmf_name, "I"), (self.vmf_area, "K")]:
-            for i, area in enumerate(area_list):
-                self.sheet_editors[self.sm.circle].sheet[f"{rowname}{i+5}"].value = self.check_area(area.text)
-
-        self.sheet_editors[self.sm.circle].update_date()
-
+        app = App.get_running_app()
+ 
+        for data in self.data:
+            for inp, rowname in [("km_input1", "D"),("km_input2", "G"), ("vmf_km_input", "J"),]:
+                app.sheet_editors[app.circle].sheet[f"{rowname}{data['index']+5}"].value = self.check_string_km(data.get(inp))
+ 
+            for inp, rowname in [("area_input1", "E"), ("area_input2", "H"),  ("vmf_input", "I"), ("vmf_area_input", "K")]:
+                #print(data.get(inp))
+                app.sheet_editors[app.circle].sheet[f"{rowname}{data['index']+5}"].value = self.check_string_area(data.get(inp))
+ 
+        app.sheet_editors[app.circle].update_date()
+ 
     def check_string_km(self, s):
         if s.strip("KM"):
             return float(s.strip("KM"))
-        else:
-            return 0
-
-    def check_area(self, s):
-        if s == "Areas Covered" or "Areas" in s or "Covered" in s:
+        return 0
+ 
+    def check_string_area(self, s):
+        if s == "Areas Covered" or "Areas" in s or "Covered" in s or "Name" in s:
             return ""
-        else:
-            return s
-
-    def check_vmf_area(self, s):
-        if s != "Area":
-            return s
-        return ""
-
-    def check_vmf_name(self, s):
-        if s != "Name":
-            return s
-        return ""
-
-
-class CircleButton(Button):
-    pass
-
-class SelectCircles(GridLayout):
-    sm = ObjectProperty(None)
-
-    def __init__(self, **kwargs):
-        """ Opening Page of the App """
-        super().__init__(**kwargs)
-        self.cols = 1
-        self.spacing = 10
-        self.padding = 40
-        self.add_widget(Label(text="TAP BELOW TO EDIT YOUR CIRCLE",
-                                color=(0,0,0,1), font_size="20sp", bold=True))
-
-        for name in range(6,12):
-            self.add_widget(CircleButton(text = f"Circle {name}"))
-
-    def clicked(self, instance):
-        logging.info("GOING TO THE INDIVIDUAL CIRCLE EDITOR")
-        self.sm.circle = instance.text
-        self.sm.current = "Next Page"
-
+        print(s)
+        return s
+ 
 
 class EditAPI:
     """ This contains all the functions to edit a cell in a sheet """
-
+ 
     def __init__(self, workbook_name, sheet_name):
         """ workbook_name = name of the workbook, sheet_name = name of the sheet to edit """
-
+ 
         self.workbook_name = workbook_name
         self.workbook = load_workbook(workbook_name)
         self.sheet = self.workbook[sheet_name]
         self.date_updated = False
         logging.info("Workbook loaded successfully")
-
-
+ 
+ 
     def update_date(self):
         """ Updates the date in a given sheet of the workbook """
-
+        today = date.today().strftime("%d-%m-%Y")
+ 
         if self.date_updated:
             logging.info("DATE ALREADY UPDATED")
-            self.workbook.save(self.workbook_name)
+            self.workbook.save(primary_ext_storage, today + "(2).xlsx")
             return
-
-        today = date.today().strftime("%d-%m-%Y")
+ 
         update_date = "Date: " + str(today)
         logging.info("Updating Date: {}".format(update_date))
         self.sheet["K2"].value = update_date
-
+ 
         self.date_updated = True
         logging.info("DATE UPDATED SUCCESSFULLY")
         filename = os.path.join(primary_ext_storage, today + "(2).xlsx")
         self.workbook.save(filename)
+ 
--- main.py
+++ main.py
@@ -4,70 +4,170 @@ from datetime import date
 from openpyxl import load_workbook
 import api
 from api import EditAPI
-from api import SelectCircles
 from api import IndividualEdit
-from api import InfoLabel, AreaInput, CircleButton
-from kivy.app import App
+ 
+from kivy.properties import StringProperty
 from kivy.core.window import Window
 from kivy.lang import Builder
-
+from kivy.app import App
+ 
+ 
 KV = """
-<InfoLabel>:
-    size_hint_x: None
-    width: 20
+<Viewclass@BoxLayout>:
+    padding: 20
+    l1: ""
+    l2: ""
+    l3: ""
+    km_input1: ""
+    area_input1: ""
+    km_input2: ""
+    area_input2: ""
+    vmf_input: ""
+    vmf_km_input: ""
+    vmf_area_input: ""
+    index: 0
+    orientation: "vertical"
+    Label:
+        text: root.l1
+        font_size : 20
+        color : (0,0,0,1)
+        bold: True
+    BoxLayout:
+        InfoLabel:
+            text: "PFM-1"
+        NameLabel:
+            text: root.l2
+        AreaInput:
+            text: root.km_input1
+            on_text:
+                app.root.ie.data[root.index]["km_input1"] = self.text
+        AreaInput:
+            text: root.area_input1
+            on_text:
+                app.root.ie.data[root.index]["area_input1"] = self.text
+    BoxLayout:
+        InfoLabel:
+            text: "PFM-2"
+        NameLabel:
+            text: root.l3
+        AreaInput:
+            text: "KM"
+            on_text:
+                app.root.ie.data[root.index]["km_input2"] = self.text
+        AreaInput:
+            text: "Areas Covered"
+            on_text:
+                app.root.ie.data[root.index]["area_input2"] = self.text
+    BoxLayout:
+        InfoLabel:
+            text: "VMF"
+        AreaInput:
+            text: "Name"
+            width: 5
+            on_text:
+                app.root.ie.data[root.index]["vmf_input"] = self.text
+        AreaInput:
+            text: "KM"
+            on_text:
+                app.root.ie.data[root.index]["vmf_km_input"] = self.text
+        AreaInput:
+            text: "Areas Covered"
+            on_text:
+                app.root.ie.data[root.index]["vmf_area_input"] = self.text
+    
+
+<NameLabel@Label>:
+    color : (0,0,0,1)
+    text_size: root.width, None
+    size: self.texture_size
+ 
+<InfoLabel@Label>:
+    size_hint_x: 0.5
     bold: True
     color: 0,0,0,1
-
-<AreaInput>:
+ 
+<AreaInput@TextInput>:
     multiline: False
-    width: 20
-
-<CircleButton>:
+ 
+<CircleButton@Button>:
     on_release:
-        self.parent.clicked(self)
+        app.circle = self.text
+        self.parent.sm.current = "Next Page"
     size_hint: .5, .5
     font_size: "20sp"
     background_color: 177/255, 126/255, 5/255, 1
-
-
+ 
+<SelectCircles@GridLayout>:
+    cols: 1
+    spacing: 10
+    padding: 40
+    Label:
+        color:(0,0,0,1)
+        font_size: "20sp"
+        bold:True
+        text: "TAP BELOW TO EDIT YOUR CIRCLE"
+    CircleButton:
+        text: "Circle 6"
+    CircleButton:
+        text: "Circle 7"
+    CircleButton:
+        text: "Circle 8"
+    CircleButton:
+        text: "Circle 9"
+    CircleButton:
+        text: "Circle 10"
+    CircleButton:
+        text: "Circle 11"
+ 
+<BackUpdateButton@Button>:
+    font_size: "20sp"
+ 
 ScreenManager:
+    ie: ie
     Screen:
         name: "Main Page"
         SelectCircles:
             sm: root
     Screen:
+        id: nextpage
         on_pre_enter:
-            ie.on_pre_enter()
+            ie.init_data()
         name: "Next Page"
         BoxLayout:
             orientation: "vertical"
             BoxLayout:
                 size_hint_y: .2
-                Button:
+                BackUpdateButton:
                     text: "Go Back"
                     on_release:
-                        ie.go_back()
-                Button:
+                        root.current = "Main Page"
+                BackUpdateButton:
                     text: "Update All"
                     on_release:
+                        print(ie.data)
                         ie.update_all()
-            ScrollView:
-                size_hint: 1, 0.8
-                IndividualEdit:
-                    id: ie
+            IndividualEdit:
+                id: ie
+                sm: root
+                viewclass: 'Viewclass'
+                RecycleBoxLayout:
+                    default_size: None, 350
+                    default_size_hint: 1, None
                     size_hint_y: None
                     height: self.minimum_height
-                    sm: root
+                    orientation: 'vertical'
 """
-
+ 
 Window.clearcolor = (135/255,206/255,235/255,1)
-
+ 
 class ReportGenerator(App):
-
+    circle = StringProperty("")
+    sheet_editors = { f"Circle {i}":EditAPI(f"4-08-2020 (2).xlsx", f"Circle {i}") for i in range(6,12) }
+ 
     def build(self):
         return Builder.load_string(KV)
-
-
+ 
+ 
 if __name__ == "__main__":
     Application = ReportGenerator
     Application().run()
